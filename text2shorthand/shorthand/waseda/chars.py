162748adbc6c1e84fd898bdf33f85b17c53327f3
Wyess <diyhacker@mail.goo.ne.jp>
2019-05-09 22:03:59 +0900

    Added CharU and related characters

--- text2shorthand/shorthand/waseda/chars.py
+++ text2shorthand/shorthand/waseda/chars.py
@@ -3,3 +3,5 @@ from ..waseda.space import CharSpace
 from ..waseda.a import CharA, CharAn, CharAku
 from ..waseda.aa import CharAa, CharAya, CharNei
 from ..waseda.i import CharI, CharIku, CharItsu
+from ..waseda.i import CharI, CharIku, CharItsu
+from ..waseda.u import CharU, CharUku, CharUn, CharKun, CharUki, CharUtsu
