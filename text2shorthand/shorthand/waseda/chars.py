eac175dd56d13a867e7ee469830600585e49b5df
Wyess <diyhacker@mail.goo.ne.jp>
2019-05-09 20:45:36 +0900

    Added CharI and other related characters

--- text2shorthand/shorthand/waseda/chars.py
+++ text2shorthand/shorthand/waseda/chars.py
@@ -2,3 +2,4 @@ from ..waseda.char import WasedaChar
 from ..waseda.space import CharSpace
 from ..waseda.a import CharA, CharAn, CharAku
 from ..waseda.aa import CharAa, CharAya, CharNei
+from ..waseda.i import CharI, CharIku, CharItsu
