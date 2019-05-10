7cf1ed762988a592fb3242db98ca8568ea219728
Wyess <diyhacker@gmail.com>
2019-05-10 20:24:06 +0900

    Initial commit

---
+++ text2shorthand/shorthand/shugiin/char.py
@@ -0,0 +1,18 @@
+import re
+import pyx
+from text2shorthand.common.point import Point as P, PPoint as PP
+from text2shorthand.common.char import Char
+
+class ShugiinChar(Char):
+    def __init__(self, name='', model='', kana='', head_type='', tail_type='', soundmark=''):
+        super().__init__(name, model, kana, head_type, tail_type)
+        self.soundmark = soundmark
+        if self.soundmark != '':
+            self.drawn_extra = False
+
+        self.tail_ligature = {'E', 'ER', 'EL', 'NE', 'NER', 'NEL', 'S', 'SL', 'SR', 'SE', 'SER', 'SEL', 'SW', 'SWR', 'SWL'}
+        self.barbs = {'', 'P'}
+        self.head_circles = {'', 'P'}
+
+    def get_paths_extra(self, **kwargs):
+        return []
---
+++ text2shorthand/shorthand/shugiin/chars.py
@@ -0,0 +1,3 @@
+from ..shugiin.char import ShugiinChar
+from ..shugiin.space import CharSpace
+from ..shugiin.a import CharA
