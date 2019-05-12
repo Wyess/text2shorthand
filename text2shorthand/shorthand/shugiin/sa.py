bb0e307e1d3e687f061faa438d51ffdf2b1227e7
Wyess <diyhacker@mail.goo.ne.jp>
2019-05-12 15:48:57 +0900

    Removed a part of tail ligature set

--- text2shorthand/shorthand/shugiin/sa.py
+++ text2shorthand/shorthand/shugiin/sa.py
@@ -15,6 +15,7 @@ class CharSa(ShugiinChar):
     def __init__(self, name='sa', kana='さ',
                  model='SR9', head_type='SR', tail_type='SR'):
         super().__init__(name, kana, model, head_type, tail_type)
+        self.tail_ligature -= {'E', 'S', 'EL', 'SEL', 'NER', 'SWL'}
 
     @classmethod
     def path_SR(cls, ta=None, **kwargs):
