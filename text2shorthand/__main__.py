e42925055f349c24c530e55ea86289c8694af752
Wyess <diyhacker@mail.goo.ne.jp>
2019-05-19 16:33:01 +0900

    9 shorthand methods are now potentially supported

--- text2shorthand/__main__.py
+++ text2shorthand/__main__.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Text-to-shorthand converter') 
     parser.add_argument('input', help='Input text file')
     parser.add_argument('output', help='Output file (.svg|.eps|.pdf)')
-    parser.add_argument('shorthand', help='Shorthand method (nakane|sangiin|shugiin|waseda')
+    parser.add_argument('shorthand', choices=['ishimura', 'kana', 'kumazaki',
+        'nakane', 'sangiin', 'shugiin', 'svsd', 'thusoku', 'waseda'], help='Shorthand method')
     args = parser.parse_args()
     app.run(shorthand=args.shorthand, in_txt=args.input, out=args.output)
--- text2shorthand/app.py
+++ text2shorthand/app.py
@@ -3,21 +3,18 @@
 from .common import context as ct
 
 def run(shorthand=None, in_txt=None, out=None):
-    if shorthand in {'nakane', 'sangiin', 'shugiin', 'svsd', 'waseda'}:
-        context = ct.Context(shorthand=f'text2shorthand.shorthand.{shorthand}.chars',
-            dic_csv=f'text2shorthand/shorthand/{shorthand}/dictionary.csv',
-            dic_merge_split_yaml=f'text2shorthand/shorthand/{shorthand}/merge_split.yaml',
-            width=150)
-        context.parse_txt_file(in_txt=in_txt)
-        context.typeset()
-        context.write_log_xml(out_xml='log.xml')
-        if out.endswith('.svg'):
-            context.write_svg_file(out)
-        elif out.endswith('.pdf'):
-            context.write_pdf_file(out)
-        elif out.endswith('.eps'):
-            context.write_eps_file(out)
-        else:
-            context.write_pdf_file(out)
+    context = ct.Context(shorthand=f'text2shorthand.shorthand.{shorthand}.chars',
+        dic_csv=f'text2shorthand/shorthand/{shorthand}/dictionary.csv',
+        dic_merge_split_yaml=f'text2shorthand/shorthand/{shorthand}/merge_split.yaml',
+        width=150)
+    context.parse_txt_file(in_txt=in_txt)
+    context.typeset()
+    context.write_log_xml(out_xml='log.xml')
+    if out.endswith('.svg'):
+        context.write_svg_file(out)
+    elif out.endswith('.pdf'):
+        context.write_pdf_file(out)
+    elif out.endswith('.eps'):
+        context.write_eps_file(out)
     else:
-        print(f'{shorthand} is not supported.')
+        context.write_pdf_file(out)
