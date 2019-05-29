8c6177ee6f0b5b4288812172cc361281ea0cfce3
Wyess <diyhacker@mail.goo.ne.jp>
2019-05-29 21:11:59 +0900

    Added a default set of tail ligatures

--- text2shorthand/shorthand/shugiin/char.py
+++ text2shorthand/shorthand/shugiin/char.py
@@ -12,6 +12,20 @@ class ShugiinChar(Char):
             self.drawn_extra = False
 
         self.tail_ligature = {'E', 'ER', 'EL', 'NE', 'NER', 'NEL', 'S', 'SL', 'SR', 'SE', 'SER', 'SEL', 'SW', 'SWR', 'SWL'}
+
+        if self.tail_type == 'E':
+            self.tail_ligature = {'E'}
+        elif self.tail_type == 'EL':
+            self.tail_ligature -= {'E', 'SR', 'S', 'EL', 'SEL', 'ER', 'NER', 'SWL'}
+        elif self.tail_type == 'ER':
+            self.tail_ligature -= {'EL', 'SEL', 'NER', 'SWL', 'S', 'E'}
+        elif self.tail_type == 'NEL':
+            self.tail_ligature -= {'SR', 'NER', 'S'}
+        elif self.tail_type == 'SEL':
+            self.tail_ligature -= {'SR', 'S', 'ER', 'NER', 'SWL'}
+        elif self.tail_type == 'S':
+            self.tail_ligature = {'S', 'SEL'}
+
         self.barbs = {'', 'P'}
         self.head_circles = {'', 'P'}
 
